f6e4ecf596dc0f5c6d550874677c822159a7eedf
shchegolenkov <denis.amorales@ya.ru>
2022-11-03 22:54:58 +0300

    create: reviews section

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 var menuButton = document.querySelector('.header-top__menu-button');
 var popupMenu = document.querySelector('.popup-menu');
+var hotel = document.querySelector('.hotel');
+var newsletter = document.querySelector('.newsletter');
 
 function popupMenuShow() { 
   popupMenu.classList.toggle('popup-menu--active')
+  hotel.classList.toggle('section--disabled')
+  newsletter.classList.toggle('section--disabled')
 };
 
 menuButton.addEventListener('click', popupMenuShow);
 
-// Swiper slider:
+// Swiper slider for hotel images:
 
-const swiper = new Swiper('.swiper', {
+const photoSlider = new Swiper('.photo-swiper', {
   // Optional parameters
   loop: true,
   speed: 500,
@@ -23,6 +27,26 @@ const swiper = new Swiper('.swiper', {
   navigation: {
     nextEl: '.photo-slider__button--next',
     prevEl: '.photo-slider__button--prev',
+  },
+
+});
+
+// Swiper slider for peviews:
+
+const reviewSlider = new Swiper('.review-swiper', {
+  // Optional parameters
+  loop: true,
+  speed: 500,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
 
 });
